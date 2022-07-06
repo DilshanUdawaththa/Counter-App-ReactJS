ef8fe8171829533578986ecbd976f95482c5e220
dilshan <dilshanudawatta15@gmail.com>
2022-07-06 15:54:01 +0530

    added ul element using rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0 };
+  state = { count: 0, tags: ["tag 01", "tag 02", "tag 03"] };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
-    let classes = "m=2 badge badge-";
+    let classes = "m-2 badge badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }

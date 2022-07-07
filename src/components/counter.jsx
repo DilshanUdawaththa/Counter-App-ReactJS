9346da36c58e4a9eaf46155ffe36e567af4e6c21
dilshan <dilshanudawatta15@gmail.com>
2022-07-08 01:54:05 +0530

    bootstrap row & col added to Counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,33 +4,38 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>
-          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button
-            onClick={() => this.props.onIncrement(this.props.counter)}
-            className="btn btn-secondary btn-sm"
-          >
-            +
-          </button>
-          <button
-            onClick={() => this.props.onDecrement(this.props.counter)}
-            className="btn btn-secondary btn-sm"
-          >
-            -
-          </button>
-          <button
-            className="btn btn-danger btn-sm"
-            onClick={() => this.props.onDelete(this.props.counter.id)}
-          >
-            Delete
-          </button>
+        <div className="row">
+          <div className="col-1">
+            <h3><span className={this.getBadgeClasses()}>{this.formatCount()}</span></h3>
+          </div>
+          <div className="col">
+            <button
+              onClick={() => this.props.onIncrement(this.props.counter)}
+              className="btn btn-secondary btn-sm"
+            >
+              +
+            </button>
+            <button
+              onClick={() => this.props.onDecrement(this.props.counter)}
+              className="btn btn-secondary btn-sm"
+              disabled={this.props.counter.value === 0 ? 'disabled' : ''}
+            >
+              -
+            </button>
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
-    let classes = "m-2 badge badge-";
+    let classes = "badge badge-";
     classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }

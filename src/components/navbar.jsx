4a8c43ea2af8fb809338993e89804b7e77bc7d18
dilshan <dilshanudawatta15@gmail.com>
2022-07-08 01:02:58 +0530

    NavBar converted into S.F.C

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,11 @@
-import React, { Component } from "react";
-
-class NavBar extends Component {
-  state = {};
-  render() {
+const NavBar = (props) => {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="www.google.com">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">{this.props.totalCount}</span>
-        </a>
-      </nav>
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="www.google.com">
+                Navbar{" "}
+                <span className="badge badge-pill badge-secondary">{props.totalCount}</span>
+            </a>
+        </nav>
     );
-  }
 }
-
 export default NavBar;

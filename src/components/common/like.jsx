3713713a548ce8173f369c3257454f608eef3c59
dilshan <dilshanudawatta15@gmail.com>
2022-07-08 02:03:42 +0530

    created Like component

---
+++ src/components/common/like.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Like extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <i className="fa-regular fa-heart"></i>
+        );
+    }
+}
+ 
+export default Like;

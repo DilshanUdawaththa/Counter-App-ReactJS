303fb9d465b4d1067da649089d699cca3fa63d62
dilshan <dilshanudawatta15@gmail.com>
2022-07-08 00:44:49 +0530

    lifted the state up (Counters --> App)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
+import React, { Component } from "react";
 import Counters from "./components/counters";
 import Movies from "./components/movies";
 import NavBar from "./components/navbar";
 
-function App() {
-  return (
-    <>
-      <NavBar />
-    <div className="container">
-      <Counters />
-      <Movies />
-    </div>
-    </>
-  );
-}
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 7 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
 
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <>
+        <NavBar />
+        <div className="container">
+          <Counters counters={this.state.counters} onReset={this.handleReset} onIncrement={this.handleIncrement} onDelete={this.handleDelete}/>
+          <Movies />
+        </div>
+      </>
+    );
+  }
+}
 export default App;
